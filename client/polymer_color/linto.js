bca71ccfd1336393dec28368eb3196ca77296e78
Linto Cheeran <linto.cet@gmail.com>
2015-04-08 21:14:45 +0530

    basic structure

---
+++ client/polymer_color/linto.js
@@ -0,0 +1,13 @@
+polymer_color_db = {};
+
+polymer_color = function(key) {
+    if (key && key != "") {
+        if (polymer_color_db[key]) return polymer_color_db[key];
+        else {
+            var A = ["_red", "_pink", "_purple", "_deep_purple", "_indigo", "_blue", "_light_blue", "_cyan", "_teal", "_green", "_light_green", "_lime", "_yellow", "_amber", "_orange", "_deep_orange", "_brown", "_grey", "_blue_grey"];
+            polymer_color_db[key] = A[Math.floor(Math.random() * A.length)];
+
+            return polymer_color_db[key];
+        }
+    } else return "";
+};
