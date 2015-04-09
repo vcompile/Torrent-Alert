69e36fbf0701d9de8b6102ec786afbd516ee42da
Linto Cheeran <linto.cet@gmail.com>
2015-04-09 08:07:45 +0530

    torrentz-list added

---
+++ public/linto/torrentz_list.js
@@ -0,0 +1,90 @@
+Polymer("torrentz-list", {
+    list: [],
+    margin: {
+        left: 4,
+        right: 4
+    },
+    swipe: 0,
+    width: 512,
+
+    domReady: function() {
+        if (512 < window.innerWidth) this.width = 512;
+        else this.width = window.innerWidth - this.margin.left - this.margin.right;
+
+        this.list = [{
+            _id: "12345",
+            data: [{
+                _id: "123",
+                category: "category",
+                categoryClass: "_red",
+                listClass: "item",
+                peers: 100,
+                seeds: 10,
+                size: "100 MB",
+                title: "item A"
+            }, {
+                _id: "123",
+                category: "category",
+                categoryClass: "_red",
+                listClass: "hidden",
+                peers: 100,
+                seeds: 10,
+                size: "100 MB",
+                title: "item B"
+            }],
+            keyword: "test B hidden group"
+        }, {
+            _id: "12345",
+            data: [{
+                _id: "123",
+                category: "category",
+                categoryClass: "_red",
+                listClass: "item",
+                peers: 100,
+                seeds: 10,
+                size: "100 MB",
+                title: "<paper-spinner active></paper-spinner>"
+            }],
+            keyword: "html entry test"
+        }];
+    },
+
+    deleteButtonTap: function(event, detail, sender) {
+        $("confirm-delete /deep/ #ok").attr("tag", $(sender).attr("tag"));
+        document.querySelector("confirm-delete /deep/ paper-action-dialog").toggle();
+    },
+
+    downloadItemDown: function(event, detail, sender) {
+        this.swipe = event.clientX;
+    },
+
+    downloadItemTrack: function(event, detail, sender) {
+        var A = this.swipe - event.clientX;
+
+        if (0 < A) $(sender).css("margin-left", (-A) + "px");
+        else $(sender).css("margin-right", A + "px");
+    },
+
+    downloadItemUp: function(event, detail, sender) {
+        this.swipe = (Math.abs(this.swipe - event.clientX) < (this.width * .5) ? 0 : 1);
+
+        $(sender).css({
+            "margin-left": 0,
+            "margin-right": 0
+        });
+    },
+
+    downloadItemTap: function(event, detail, sender) {
+        if (this.swipe) console.log("swipe", $(sender).attr("tag"));
+        else console.log("tap", $(sender).attr("tag"));
+
+    },
+
+    listChanged: function() {
+        if (this.list instanceof Array) $("#torrent_out_db").val(JSON.stringify(this.list));
+        else {
+            this.list = JSON.parse(this.list);
+            $("#torrent_out_db").val(JSON.stringify(this.list));
+        }
+    }
+});
--- public/linto/torrentz_menu.js
+++ public/linto/torrentz_menu.js
@@ -15,8 +15,11 @@ Polymer("torrentz-menu", {
     },
 
     listChanged: function() {
-        if (this.list instanceof Array)
+        if (this.list instanceof Array) $("#torrent_in_db").val(JSON.stringify(this.list));
+        else {
+            this.list = JSON.parse(this.list);
             $("#torrent_in_db").val(JSON.stringify(this.list));
+        }
     },
 
     menuItemRelease: function(event, detail, sender) {
@@ -25,30 +28,6 @@ Polymer("torrentz-menu", {
     },
 
     menuItemTap: function(event, detail, sender) {
-        var _id = $(sender).attr("tag");
-
-        var head = JSON.parse($("#torrent_in_db").val()),
-            list = JSON.parse($("#torrent_out_db").val());
-
-        var index = -1;
-
-        var item = _.find(head, function(item) {
-            index++;
-
-            return (item["_id"] == _id);
-        });
-
-        if (typeof(item) != "undefined" || item != null) {
-            if (list[index] && list[index].length) {
-                $("torrentz-list-item").attr("head", JSON.stringify([item]));
-                $("torrentz-list-item").attr("list", JSON.stringify([list[index]]));
-
-                document.querySelector("core-animated-pages").selected = 2;
-            } else {
-                // $("").attr("tag", _id);
-
-                document.querySelector("core-animated-pages").selected = 3;
-            }
-        }
+        console.log($(sender).attr("tag"));
     }
 });
