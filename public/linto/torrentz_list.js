5de14c4403ce5ab9fa7d9612b45bce4a753a7ce7
Linto Cheeran <linto.cet@gmail.com>
2015-04-11 02:12:58 +0530

    webkit highlight off

--- public/linto/torrentz_list.js
+++ public/linto/torrentz_list.js
@@ -17,23 +17,37 @@ Polymer("torrentz-list", {
         else this.width = window.innerWidth - this.margin.left - this.margin.right;
     },
 
-    downloadItemDown: function(event, detail, sender) {
-        this.swipe = event.clientX;
-    },
-
     downloadItemTap: function(event, detail, sender) {
         var _id = $(sender).attr("tag");
 
-        if (1 < this.swipe) {
-            this.swipe = (Math.abs(this.swipe - event.clientX) < 80) ? 0 : 1;
+        $("download-linkz").attr("body", "<div center-justified horizontal layout><paper-spinner active></paper-spinner></div>");
+        $("download-linkz").attr("header", $(sender).html());
 
-            $(sender).css({
-                "margin-left": 0,
-                "margin-right": 0
-            });
-        }
+        document.querySelector("download-linkz /deep/ paper-action-dialog").toggle();
 
-        if (this.swipe) {
+        Meteor.call("downloadLinkz", _id, function(error, linkz) {
+            if (error) toast(error.reason);
+            else {
+                var body = "";
+
+                linkz.forEach(function(item) {
+                    body += '<div class="download-link menu-l" horizontal layout onclick="window.open(\'' + item.url + '\');"><div self-center><div class="' + polymer_color(item.text) + ' menu-l-icon-text">' + (isNaN(item.text.charAt(0)) ? item.text.charAt(0).toUpperCase() : "#") + '</div></div><div class="menu-l-description" flex layout self-center vertical><div auto-vertical horizontal layout>' + item.text + '</div><div auto-vertical class="menu-l-description-small">' + item.fromNow + '</div></div></div>';
+                });
+
+                $("download-linkz").attr("body", body);
+            }
+        });
+    },
+
+    downloadItemTrack: function(event, detail, sender) {
+        var A = this.swipe - event.clientX;
+
+        if (0 < A) $(sender).css("margin-left", (-A) + "px");
+        else $(sender).css("margin-right", A + "px");
+
+        if ((this.width * .25) < A) {
+            var _id = $(sender).attr("tag");
+
             for (var A = 0; A < torrentz_db.length; A++) {
                 var index = -1;
 
@@ -59,60 +73,18 @@ Polymer("torrentz-list", {
                     break;
                 }
             }
-        } else {
-            $("download-linkz").attr("body", "<div center-justified horizontal layout><paper-spinner active></paper-spinner></div>");
-            $("download-linkz").attr("header", $(sender).html());
-
-            document.querySelector("download-linkz /deep/ paper-action-dialog").toggle();
-
-            Meteor.call("downloadLinkz", _id, function(error, linkz) {
-                if (error) toast(error.reason);
-                else {
-                    var body = "";
-
-                    linkz.forEach(function(item) {
-                        body += '<div class="download-link menu-l" horizontal layout onclick="window.open(\'' + item.url + '\');"><div self-center><div class="' + polymer_color(item.text) + ' menu-l-icon-text">' + (isNaN(item.text.charAt(0)) ? item.text.charAt(0).toUpperCase() : "#") + '</div></div><div class="menu-l-description" flex layout self-center vertical><div auto-vertical horizontal layout>' + item.text + '</div><div auto-vertical class="menu-l-description-small">' + item.fromNow + '</div></div></div>';
-                    });
-
-                    $("download-linkz").attr("body", body);
-                }
-            });
-        }
-    },
-
-    downloadItemTrack: function(event, detail, sender) {
-        var A = this.swipe - event.clientX;
-
-        if (A < 5) {
-            if (0 < A) $(sender).css("margin-left", (-A) + "px");
-            else $(sender).css("margin-right", A + "px");
         }
     },
 
     downloadItemTrackend: function(event, detail, sender) {
-        if (1 < this.swipe) {
-            this.swipe = (Math.abs(this.swipe - event.clientX) < 80) ? 0 : 1;
-
-            $(sender).css({
-                "margin-left": 0,
-                "margin-right": 0
-            });
-        }
+        $(sender).css({
+            "margin-left": 0,
+            "margin-right": 0
+        });
     },
 
     downloadItemTrackstart: function(event, detail, sender) {
         this.swipe = event.clientX;
-    },
-
-    downloadItemUp: function(event, detail, sender) {
-        if (1 < this.swipe) {
-            this.swipe = (Math.abs(this.swipe - event.clientX) < 80) ? 0 : 1;
-
-            $(sender).css({
-                "margin-left": 0,
-                "margin-right": 0
-            });
-        }
     },
 
     listChanged: function() {
