d480e21180f0912b82dc782fd16ff81fd6013fa1
Linto Cheeran <linto.cet@gmail.com>
2015-04-24 19:12:01 +0530

    specialUser Access

--- collections.js
+++ collections.js
@@ -1,3 +1,33 @@
+currentUser = function(userId, doc) {
+    return (!!userId) && doc.user_id === userId;
+};
+
+grantAllAccess = function(userId, doc) {
+    return true;
+};
+
+specialAccess = function(userId, doc) {
+    return userId == "linto";
+};
+
 torrent_in = new Meteor.Collection("torrent_in");
 torrent_out = new Meteor.Collection("torrent_out");
 torrent_push = new Meteor.Collection("torrent_push");
+
+torrent_in.allow({
+    insert: specialAccess,
+    remove: specialAccess,
+    update: specialAccess
+});
+
+torrent_out.allow({
+    insert: specialAccess,
+    remove: specialAccess,
+    update: specialAccess
+});
+
+torrent_out.allow({
+    insert: specialAccess,
+    remove: specialAccess,
+    update: specialAccess
+});
--- server/publications.js
+++ server/publications.js
@@ -1,39 +1,47 @@
 Meteor.publish("torrent_in", function() {
-    return torrent_in.find({
-        user_id: this.userId
-    }, {
-        fields: {
-            keyword: 1,
-            peers: 1,
-            seeds: 1,
-            url: 1,
-            urlPart: 1
-        },
-        sort: {
-            time: -1
-        }
-    });
+    if (this.userId == "linto") {
+        return torrent_in.find();
+    } else {
+        return torrent_in.find({
+            user_id: this.userId
+        }, {
+            fields: {
+                keyword: 1,
+                peers: 1,
+                seeds: 1,
+                url: 1,
+                urlPart: 1
+            },
+            sort: {
+                time: -1
+            }
+        });
+    }
 });
 
 Meteor.publish("torrent_out", function(query) {
-    return torrent_out.find({
-        torrent_in: {
-            $in: query.torrent_in
-        }
-    }, {
-        fields: {
-            category: 1,
-            peers: 1,
-            seeds: 1,
-            size: 1,
-            time: 1,
-            title: 1,
-            torrent_in: 1,
-            url: 1,
-            verified: 1
-        },
-        sort: {
-            time: -1
-        }
-    });
+    if (this.userId == "linto") {
+        return torrent_out.find();
+    } else {
+        return torrent_out.find({
+            torrent_in: {
+                $in: query.torrent_in
+            }
+        }, {
+            fields: {
+                category: 1,
+                peers: 1,
+                seeds: 1,
+                size: 1,
+                time: 1,
+                title: 1,
+                torrent_in: 1,
+                url: 1,
+                verified: 1
+            },
+            sort: {
+                time: -1
+            }
+        });
+    }
 });
