4a00b653808c84f29b43d526da2eacfcf4c40962
Linto Cheeran <linto.cet@gmail.com>
2015-04-25 18:12:59 +0530

    fixes

--- collections.js
+++ collections.js
@@ -1,33 +1,33 @@
-currentUser = function(userId, doc) {
-    return (!!userId) && doc.user_id === userId;
-};
+// currentUser = function(userId, doc) {
+//     return (!!userId) && doc.user_id === userId;
+// };
 
-grantAllAccess = function(userId, doc) {
-    return true;
-};
+// grantAllAccess = function(userId, doc) {
+//     return true;
+// };
 
-specialAccess = function(userId, doc) {
-    return userId == "HedCET";
-};
+// specialAccess = function(userId, doc) {
+//     return userId == "HedCET";
+// };
 
 torrent_in = new Meteor.Collection("torrent_in");
 torrent_out = new Meteor.Collection("torrent_out");
 torrent_push = new Meteor.Collection("torrent_push");
 
-torrent_in.allow({
-    insert: specialAccess,
-    remove: specialAccess,
-    update: specialAccess
-});
+// torrent_in.allow({
+//     insert: specialAccess,
+//     remove: specialAccess,
+//     update: specialAccess
+// });
 
-torrent_out.allow({
-    insert: specialAccess,
-    remove: specialAccess,
-    update: specialAccess
-});
+// torrent_out.allow({
+//     insert: specialAccess,
+//     remove: specialAccess,
+//     update: specialAccess
+// });
 
-torrent_out.allow({
-    insert: specialAccess,
-    remove: specialAccess,
-    update: specialAccess
-});
+// torrent_push.allow({
+//     insert: specialAccess,
+//     remove: specialAccess,
+//     update: specialAccess
+// });
