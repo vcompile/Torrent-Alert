e7d951d9e21d9ef4b63adab8911561a1a39f1f12
Linto Cheeran <linto.cet@gmail.com>
2015-06-11 02:31:50 +0530

    fixes

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+    id: "com.vcompile.torrentz",
+    name: "Torrentz Alert",
+    description: "Torrentz Watcher & Alerter",
+    author: "Linto Cheeran",
+    email: "linto.cet@gmail.com",
+    website: "http://github.com/HedCET"
+});
--- public/linto/menu_b.js
+++ public/linto/menu_b.js
@@ -6,18 +6,22 @@ Polymer("menu-b", {
 
     addIconTap: function() {
         if (Meteor.user()) {
-            document.querySelector("add-keyword /deep/ paper-action-dialog").open();
+            document.querySelector("c-panel /deep/ paper-action-dialog").open();
         } else {
-            if (Accounts.loginServicesConfigured()) {
-                Meteor.loginWithGoogle({
-                    requestOfflineToken: true,
-                    requestPermissions: ["email", "profile"]
-                }, function(error) {
-                    if (error) {
-                        $("toast-handler").attr("text", Accounts.LoginCancelledError.numericError);
-                        $("toast-handler").attr("undo_hidden_callback_opt", "");
-                    } else location.reload();
+            if (Meteor.isCordova) {
+                Meteor.cordovaSignIn({
+                    cordova_g_plus: true
                 });
+            } else {
+                if (Accounts.loginServicesConfigured()) {
+                    Meteor.loginWithGoogle({
+                        requestOfflineToken: true,
+                        requestPermissions: ["email", "profile"]
+                    }, function(error) {
+                        if (error) toast(Accounts.LoginCancelledError.numericError);
+                        else location.reload();
+                    });
+                }
             }
         }
     },
