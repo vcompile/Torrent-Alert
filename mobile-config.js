0426a17d3e3694e9af210e7fe1451fdf9ca87a5f
Linto Cheeran <linto.cet@gmail.com>
2015-06-13 00:38:03 +0530

    fixes

--- mobile-config.js
+++ mobile-config.js
@@ -2,6 +2,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrentz Alert",
     description: "Torrentz Keyword Watcher & Alerter",
+    version: "0.0.1",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://github.com/HedCET"
