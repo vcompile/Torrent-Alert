94014aadb994e289e3aeac42a4a5e7a00284e192
Linto Cheeran <linto.cet@gmail.com>
2015-07-12 08:25:51 +0530

    fixes

--- client/config.cordova.js
+++ client/config.cordova.js
@@ -1,29 +1,33 @@
 if (Meteor.isCordova) {
-    document.addEventListener("deviceready", function() { // $(document).on();
+    document.addEventListener("deviceready", function() {
 
         document.addEventListener("backbutton", backbutton, false);
         document.addEventListener("menubutton", menubutton, false);
 
         if (navigator.connection.type == "none") {
-            // $("toast-handler").attr("text", "No internet connection");
-            // $("toast-handler").attr("undo_hidden_callback_opt", "");
+            if (document.querySelector("html /deep/ user-auth")) {
+                document.querySelector("html /deep/ user-auth").toastText = "No internet connection";
+                document.querySelector("html /deep/ user-auth").toastOpened = true;
+            }
         }
 
     }, false);
 
     function backbutton() {
-        // if ($("html /deep/ #drawer-panel").width() < 768) {
-        //     document.querySelector("html /deep/ #drawer-panel").closeDrawer();
-        // }
+        if ($("html /deep/ #drawerPanel").width() <= 768) {
+            document.querySelector("html /deep/ #drawerPanel").closeDrawer();
+        }
 
-        // for (var A = document.querySelectorAll("html /deep/ paper-action-dialog"), B = 0; B < A.length; B++) {
-        //     A[B].close();
-        // }
+        for (var A = document.querySelectorAll("html /deep/ paper-dialog"), B = 0; B < A.length; B++) {
+            A[B].close();
+        }
 
-        // $("confirm-exit").attr("time", moment().format("x"));
+        $("html /deep/ confirm-exit").attr("time", moment().format("x"));
     }
 
     function menubutton() {
-        // document.querySelector("#drawer-panel").togglePanel();
+        if (document.querySelector("html /deep/ inbox-list")) {
+            document.querySelector("html /deep/ inbox-list").forceNarrowTap();
+        }
     }
 }
