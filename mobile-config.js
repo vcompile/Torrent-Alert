d74457597590f74cce3601cb8f060d5d2d8e0355
Linto Cheeran <linto.cet@gmail.com>
2015-07-27 14:42:35 +0530

    release 0.0.15

--- mobile-config.js
+++ mobile-config.js
@@ -1,3 +1,12 @@
+App.accessRule("*://*.googleusercontent.com/*");
+
+App.icons({
+    "android_ldpi": ".build/drawable-ldpi/ic_launcher.png",
+    "android_mdpi": ".build/drawable-mdpi/ic_launcher.png",
+    "android_hdpi": ".build/drawable-hdpi/ic_launcher.png",
+    "android_xhdpi": ".build/drawable-xhdpi/ic_launcher.png"
+});
+
 App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
@@ -7,3 +16,16 @@ App.info({
     email: "linto@vcompile.com",
     website: "http://github.com/HedCET"
 });
+
+// App.launchScreens({
+//     "android_ldpi_portrait": ".build/splash/splash-200x320.png",
+//     "android_ldpi_landscape": ".build/splash/splash-320x200.png",
+//     "android_mdpi_portrait": ".build/splash/splash-320x480.png",
+//     "android_mdpi_landscape": ".build/splash/splash-480x320.png",
+//     "android_hdpi_portrait": ".build/splash/splash-480x800.png",
+//     "android_hdpi_landscape": ".build/splash/splash-800x480.png",
+//     "android_xhdpi_portrait": ".build/splash/splash-720x1280.png",
+//     "android_xhdpi_landscape": ".build/splash/splash-1280x720.png"
+// });
+
+// App.setPreference("android-installLocation", "preferExternal");
