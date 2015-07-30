eece5227a8f2866776a722eb91d27825b9b04ad2
Linto Cheeran <linto.cet@gmail.com>
2015-07-30 12:03:57 +0530

    fixes

--- client/polymer_color/linto.js
+++ client/polymer_color/linto.js
@@ -1,5 +1,64 @@
 polymer_color_db = {};
 
+polymer_color_opt = [{
+    class: "_red",
+    count: 0
+}, {
+    class: "_pink",
+    count: 0
+}, {
+    class: "_purple",
+    count: 0
+}, {
+    class: "_deep_purple",
+    count: 0
+}, {
+    class: "_indigo",
+    count: 0
+}, {
+    class: "_blue",
+    count: 0
+}, {
+    class: "_light_blue",
+    count: 0
+}, {
+    class: "_cyan",
+    count: 0
+}, {
+    class: "_teal",
+    count: 0
+}, {
+    class: "_green",
+    count: 0
+}, {
+    class: "_light_green",
+    count: 0
+}, {
+    class: "_lime",
+    count: 0
+}, {
+    class: "_yellow",
+    count: 0
+}, {
+    class: "_amber",
+    count: 0
+}, {
+    class: "_orange",
+    count: 0
+}, {
+    class: "_deep_orange",
+    count: 0
+}, {
+    class: "_brown",
+    count: 0
+}, {
+    class: "_grey",
+    count: 0
+}, {
+    class: "_blue_grey",
+    count: 0
+}];
+
 polymer_color = function(key) {
     if (key) {
         key = key.replace(/[^0-9a-zA-Z]/g, "");
@@ -7,10 +66,12 @@ polymer_color = function(key) {
         if (key.length) {
             if (polymer_color_db[key]) return polymer_color_db[key];
             else {
-                var A = ["_red", "_pink", "_purple", "_deep_purple", "_indigo", "_blue", "_light_blue", "_cyan", "_teal", "_green", "_light_green", "_lime", "_yellow", "_amber", "_orange", "_deep_orange", "_brown", "_grey", "_blue_grey"];
-                polymer_color_db[key] = A[Math.floor(Math.random() * A.length)];
+                var A = _.sortBy(polymer_color_opt, "count");
 
-                return polymer_color_db[key];
+                polymer_color_db[key] = A[0].class;
+                A[0].count++;
+
+                return A[0].class;
             }
         } else return "";
     } else return "";
