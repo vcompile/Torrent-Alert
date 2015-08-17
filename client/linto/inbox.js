090ded475349fc5762bd093ab52f337dfa785cc5
Linto Cheeran <linto.cet@gmail.com>
2015-08-17 09:11:55 +0530

    rewrite inbox-list to layout-inbox almost completed

--- client/linto/inbox.js
+++ client/linto/inbox.js
@@ -1,5 +1,12 @@
 Template.inbox.helpers({
     width: function() {
-        return (480 < $(window).width()) ? 480 : ($(window).width() - 32);
+        if (Session.get("width") && !isNaN(Session.get("width"))) return Session.get("width");
+        else return (480 < $(window).width()) ? 480 : ($(window).width() - 32);
     }
 });
+
+Template.inbox.rendered = function() {
+    $(window).on("resize", _.debounce(function() {
+        Session.set("width", ((480 < $(window).width()) ? 480 : ($(window).width() - 32)));
+    }, 800));
+};
