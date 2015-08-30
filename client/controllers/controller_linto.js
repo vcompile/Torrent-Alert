e4aef3c504cde73af8fd77c6a5a6f0663d06b1ff
Linto Cheeran <linto.cet@gmail.com>
2015-08-30 21:48:02 +0530

    fixes

--- client/controllers/controller_linto.js
+++ client/controllers/controller_linto.js
@@ -1,4 +1,9 @@
 Template.controller_linto.helpers({
+    deny: function() {
+        if (Meteor.userId() == "HedCET") return true;
+        else return false;
+    },
+
     view: function() {
         if (Session.get("resetPasswordToken") && (Session.get("resetPasswordToken") != "")) return "resetPassword";
         else return Session.get("view") ? Session.get("view") : "signIn";
