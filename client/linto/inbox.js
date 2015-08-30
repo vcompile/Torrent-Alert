f802aad1fa121e07eef900f3f02111fd0716470e
Linto Cheeran <linto.cet@gmail.com>
2015-08-30 23:41:52 +0530

    pagination added

--- client/linto/inbox.js
+++ client/linto/inbox.js
@@ -1,12 +1,12 @@
 Template.inbox.helpers({
     width: function() {
         if (Session.get("width") && !isNaN(Session.get("width"))) return Session.get("width");
-        else return (480 < $(window).width()) ? 480 : ($(window).width() - 32);
+        else return (480 < $(window).width()) ? 480 : ($(window).width() - 16);
     }
 });
 
 Template.inbox.rendered = function() {
     $(window).on("resize", _.debounce(function() {
-        Session.set("width", ((480 < $(window).width()) ? 480 : ($(window).width() - 32)));
+        Session.set("width", ((480 < $(window).width()) ? 480 : ($(window).width() - 16)));
     }, 400));
 };
--- server/main.js
+++ server/main.js
@@ -1,3 +1,4 @@
-crontab_time = ["09:00", "21:00"];
 fibers = Npm.require("fibers");
-ping_interval = 30; // in min
+
+crontab_time = ["09:00", "21:00"];
+ping_interval = 60; // in min
