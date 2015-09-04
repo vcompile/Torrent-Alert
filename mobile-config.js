9e6cade6129142ae2f8b62c8706f3b309e68f317
Linto Cheeran <linto.cet@gmail.com>
2015-09-05 00:47:51 +0530

    paper-fab fises

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
     description: "Torrentz Keyword Watcher & Alerter",
-    version: "1.2.3",
+    version: "1.3.0",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://github.com/HedCET"
