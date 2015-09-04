79a4dd14f403f2cbcbecc526355d4e7903b7d8d3
Linto Cheeran <linto.cet@gmail.com>
2015-09-04 09:38:05 +0530

    release 1.2.3

--- client/polymer_color/linto.js
+++ client/polymer_color/linto.js
@@ -1,4 +1,4 @@
-polymer_color_class = ["red-500", "pink-500", "purple-500", "deep-purple-500", "indigo-500", "blue-500", "light-blue-500", "cyan-500", "teal-500", "green-500", "light-green-500", "lime-500", "yellow-500", "amber-500", "orange-500", "deep-orange-500", "brown-500", "grey-500", "blue-grey-500"];
+polymer_color_class = ["red-500", "pink-500", "purple-500", "deep-purple-500", "indigo-500", "blue-500", "light-blue-500", "cyan-500", /* "teal-500", */ "green-500", "light-green-500", "lime-500", "yellow-500", "amber-500", "orange-500", "deep-orange-500", "brown-500", /* "grey-500", */ "blue-grey-500"];
 polymer_color_local = {};
 
 polymer_color = function(key, save) {
--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
     description: "Torrentz Keyword Watcher & Alerter",
-    version: "1.2.2",
+    version: "1.2.3",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://github.com/HedCET"
