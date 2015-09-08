e07d47a20b3d75530a2e865b830676c744d852bb
Linto Cheeran <linto.cet@gmail.com>
2015-09-08 17:36:53 +0530

    release 1.3.2

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
     description: "Torrentz Keyword Watcher & Alerter",
-    version: "1.3.1",
+    version: "1.3.2",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://github.com/HedCET"
