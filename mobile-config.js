10cb38e558a0903bd803f8098e8573f78204af1b
HedCET <linto.cet@gmail.com>
2015-09-26 15:52:58 +0530

    version 1.5.3

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
     description: "Torrent Keyword Watcher & Alerter",
-    version: "1.5.2",
+    version: "1.5.3",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://github.com/HedCET"
