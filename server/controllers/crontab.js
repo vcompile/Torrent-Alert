0e3d6ca87c905a2ce5f3d1d27fb00f5dbfd38e40
HedCET <linto.cet@gmail.com>
2015-11-03 23:26:10 +0530

    scheduler added

--- server/controllers/crontab.js
+++
@@ -1,7 +0,0 @@
-Meteor.setInterval(function() {
-    new fibers(function() {
-        torrent_in.find().fetch().forEach(function(item) {
-            torrent_in_http_proxy_request(item);
-        });
-    }).run();
-}, 1000 * 60 * 60 * 6);
---
+++ server/controllers/sheduler.js
@@ -0,0 +1,29 @@
+Meteor.setInterval(function() {
+    new fibers(function() {
+        _project.find().fetch().forEach(function(item) {
+            if (item.worker == "schedule" && 6 < moment.duration(moment().diff(moment(item.time))).asHours()) {
+                if (!_worker.findOne({
+                        project: item._id,
+                        status: "",
+                        type: item.worker
+                    })) {
+                    _worker.insert({
+                        project: item._id,
+                        status: "",
+                        time_insert: moment().format(),
+                        type: item.worker,
+                        user: item.user
+                    });
+                }
+
+                _project.update({
+                    _id: item._id
+                }, {
+                    $set: {
+                        time: moment().format()
+                    }
+                });
+            }
+        });
+    }).run();
+}, 1000 * 60 * 60);
