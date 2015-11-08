8537b9df5eed45ac5ae3d1747562ed61122fc26f
HedCET <linto.cet@gmail.com>
2015-11-08 15:10:13 +0530

    raix:push fixes

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
     id: "com.vcompile.torrentz",
     name: "Torrent Alert",
     description: "Torrent Keyword Watcher & Alerter",
-    version: "1.7.0",
+    version: "1.7.1",
     author: "Linto Cheeran",
     email: "linto.cet@gmail.com",
     website: "http://vcompile.com/torrentz"
