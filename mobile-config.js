f7597823dd2ceaa17c1791a6cd6e67a14c7851ea
Linto Cheeran <linto.cet@gmail.com>
2016-02-04 02:10:35 +0530

    TorrentAlert-1.9.6

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.info({
   id: "com.vcompile.torrentz",
   name: "Torrent Alert",
   description: "Torrent Search, Keyword Scheduler & Alerter",
-  version: "1.9.5",
+  version: "1.9.6",
   author: "LintoCheeran",
   email: "linto.cet@gmail.com",
   website: "http://vcompile.com/torrentz"
