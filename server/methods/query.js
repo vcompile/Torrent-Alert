df58ea71f93f194f67507eddb1dc6f1216420cbd
Linto Cheeran <linto.cet@gmail.com>
2016-07-11 14:23:12 +0530

    try{}catch(){} added

--- server/methods/query.js
+++ server/methods/query.js
@@ -8,58 +8,63 @@ Meteor.methods({
 
     check(query, String);
 
-    var req = HTTP.call("GET", Random.choice(_torrentz_proxy) + query, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
-      },
-      npmRequestOptions: {
-        proxy: Random.choice(_proxy),
-      },
-      timeout: 1000 * 60,
-    });
     var res = {};
 
-    if (req.statusCode === 200) {
-      var $ = Meteor.npmRequire("cheerio").load(req.content);
+    try {
+      var req = HTTP.call("GET", Random.choice(_torrentz_proxy) + query, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+        },
+        npmRequestOptions: {
+          proxy: Random.choice(_proxy),
+        },
+        timeout: 1000 * 60,
+      });
 
-      if (-1 < query.indexOf('?')) { // torrent
-        res.count = +_.first($('.results h2').children().remove().end().text().split(/torrents/i)).replace(/[^0-9]/g, '');
-        res.torrent = [];
+      if (req.statusCode === 200) {
+        var $ = Meteor.npmRequire("cheerio").load(req.content);
 
-        $(".results dl").each(function() {
-          if ($(this).find("dt a").attr("href")) {
-            res.torrent.push({
+        if (-1 < query.indexOf('?')) { // torrent
+          res.count = +_.first($('.results h2').children().remove().end().text().split(/torrents/i)).replace(/[^0-9]/g, '');
+          res.torrent = [];
 
-              title: $(this).find("dt a").text(),
-              query: $(this).find("dt a").attr("href"),
-              category: $(this).find("dt").children().remove().end().text().replace(/[^0-9a-z ]/gi, " ").trim().replace(/\s+/g, " "),
+          $(".results dl").each(function() {
+            if ($(this).find("dt a").attr("href")) {
+              res.torrent.push({
 
-              insert_time: ($(this).find("dd .a span").attr("title") ? moment($(this).find("dd .a span").attr("title"), "ddd, DD MMM YYYY HH:mm:ss").toDate() : moment().toDate()),
-              leech: +$(this).find("dd .u").text().replace(/[^0-9]/g, ""),
-              seed: +$(this).find("dd .d").text().replace(/[^0-9]/g, ""),
-              size: $(this).find("dd .s").text().replace(/[^0-9a-z]/gi, ""),
+                title: $(this).find("dt a").text(),
+                query: $(this).find("dt a").attr("href"),
+                category: $(this).find("dt").children().remove().end().text().replace(/[^0-9a-z ]/gi, " ").trim().replace(/\s+/g, " "),
 
-            });
-          }
-        });
-      } else { // url
-        res.url = [];
+                insert_time: ($(this).find("dd .a span").attr("title") ? moment($(this).find("dd .a span").attr("title"), "ddd, DD MMM YYYY HH:mm:ss").toDate() : moment().toDate()),
+                leech: +$(this).find("dd .u").text().replace(/[^0-9]/g, ""),
+                seed: +$(this).find("dd .d").text().replace(/[^0-9]/g, ""),
+                size: $(this).find("dd .s").text().replace(/[^0-9a-z]/gi, ""),
 
-        $(".download dl").each(function() {
-          if ($(this).find("dt a").attr("href")) {
-            var url = $(this).find("dt a").attr("href");
-
-            if (url.substr(0, 4) == "http") {
-              res.url.push({
-                insert_time: ($(this).find("dd span").attr("title") ? moment($(this).find("dd span").attr("title"), "ddd, DD MMM YYYY HH:mm:ss").toDate() : moment().toDate()),
-                url: url,
               });
             }
-          }
-        });
+          });
+        } else { // url
+          res.url = [];
+
+          $(".download dl").each(function() {
+            if ($(this).find("dt a").attr("href")) {
+              var url = $(this).find("dt a").attr("href");
+
+              if (url.substr(0, 4) == "http") {
+                res.url.push({
+                  insert_time: ($(this).find("dd span").attr("title") ? moment($(this).find("dd span").attr("title"), "ddd, DD MMM YYYY HH:mm:ss").toDate() : moment().toDate()),
+                  url: url,
+                });
+              }
+            }
+          });
+        }
+      } else {
+        console.log('query HTTP.call()', query);
       }
-    } else {
-      console.log('query HTTP.call()', query);
+    } catch (e) {
+      console.log(e);
     }
 
     return res;
