43bdecee258c570667c4c65b6ffd773181caee2e
Linto Cheeran <linto.cet@gmail.com>
2016-07-11 15:28:26 +0530

    push test

---
+++ server/test.js
@@ -0,0 +1,13 @@
+Meteor.setTimeout(function() {
+  Push.send({
+    from: "com.vcompile.torrentz",
+    notId: 1,
+    payload: {
+      torrent: ['LpMui7PGymSWDd3gA'],
+      project: 'jevd7rX295kscNtsB',
+    },
+    query: {},
+    text: '1 item',
+    title: 'malayalam',
+  });
+}, 1000 * 4);
