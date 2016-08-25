cce9735de0c381c5b1b347e2f0dea68ca927bc24
Linto Cheeran <linto.cet@gmail.com>
2016-08-25 15:15:03 +0530

    latest to collection

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,33 +1,4 @@
 if (!_project.findOne({
-    _id: 'latest_movies',
-  }, {
-    fields: {
-      _id: 1,
-    },
-  })) {
-  _project.insert({
-    _id: 'latest_movies',
-    index: 3,
-    query: '/search?f=movies',
-    title: 'Latest Movie',
-    worker: 'schedule',
-  });
-
-  _worker.upsert({
-    input: 'latest_movies',
-    status: '',
-    type: 'schedule',
-  }, {
-    $set: {
-      input: 'latest_movies',
-      insert_time: moment().toDate(),
-      status: '',
-      type: 'schedule',
-    },
-  });
-}
-
-if (!_project.findOne({
     _id: 'latest_softwares',
   }, {
     fields: {
@@ -38,7 +9,7 @@ if (!_project.findOne({
     _id: 'latest_softwares',
     index: 2,
     query: '/search?f=software',
-    title: 'Latest App',
+    title: 'App Collection',
     worker: 'schedule',
   });
 
